dc5f8ed9bbd9fa77ba6f17739a77d150bc8cc445
Shreyash-F090 <Shreyashmkadam307@gmail.com>
2026-07-16 13:36:24 +0530

    Add files via upload

---
+++ OSP2A.py
@@ -0,0 +1,31 @@
+from multiprocessing import Process, Queue
+import time
+import random
+
+def producer(queue):
+    for i in range(5):
+        item = random.randint(1, 100)
+
+        if queue.full():
+            print("Queue is full! Producer is waiting...")
+
+        queue.put(item)
+        print(f"Producer produced: {item}")
+        time.sleep(random.random())
+
+def consumer(queue):
+    for i in range(5):
+        item = queue.get()
+        print(f"Consumer consumed: {item}")
+        time.sleep(random.random())
+
+if __name__ == "__main__":
+    print("Shreyash Kadam S091")
+    q = Queue(maxsize=3)
+    p1 = Process(target=producer, args=(q,))
+    p2 = Process(target=consumer, args=(q,))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+    print("Producer and Consumer have finished.")
